109835680b4234b06d8c99b4348ffb10eff90a6c
Daniel-n-huss <daniel.n.huss@gmail.com>
2020-06-10 06:59:55 -0600

    header component and story written

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.scss'
+import Header from 'components/Appointment/Header'
+
+
+const Appointment = function (props) {
+
+return <article className="appointment"></article>
+};
+
+
+
+export default Appointment;
